ab6ba3dae074e8ee18df062d25d14c600c74e1d0
Aditya <adityakarnam@outlook.com>
2014-12-18 22:49:29 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*  jQuery ready function. Specify a function to execute when the DOM is fully loaded.  */
+$(document).ready(
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+    /* Next part of code handles hovering effect and submenu appearing */
+    $('.nav li').hover(
+      function () { //appearing on hover
+        $('ul', this).fadeIn();
+      },
+      function () { //disappearing on hover
+        $('ul', this).fadeOut();
+      }
+    );
+  }
+);
+
+$(function() {
+options = {
+ elements :'.btn',
+ focus :'.btn',
+ focusTimeOut: 600
+};
+rippleEffect = new $.RippleEffect(options);
+});
